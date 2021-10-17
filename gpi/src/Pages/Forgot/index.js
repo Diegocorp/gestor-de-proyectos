add2afc243049a36e16721745e5b3d74ab49ad52
Diegocorp <diegosandovalr.corp@gmail.com>
2021-10-17 13:53:28 -0700

    Responsive forgot password page fixed

--- gpi/src/Pages/Forgot/index.js
+++ gpi/src/Pages/Forgot/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import TecLogo from '../../Assets/img/tecnm-1.png';
-import { Link } from 'react-router-dom';
-import '../Login/styles.css';
+import React from "react";
+import TecLogo from "../../Assets/img/tecnm-1.png";
+import { Link } from "react-router-dom";
+import "../Login/styles.css";
 
-function Forgot(){
-  return(
+function Forgot() {
+  return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-9 col-lg-12 col-xl-10">
@@ -12,36 +12,48 @@ function Forgot(){
             <div className="card-body p-0">
               <div className="row">
                 <div className="col-lg-5 d-none d-lg-flex">
-                  <img id="iconLogo" src={TecLogo} alt=""/>
+                  <img id="iconLogo" src={TecLogo} alt="" />
                 </div>
                 <div className="col-lg-6">
                   <div className="p-5">
                     <div className="text-center">
-                      <h4 className="text-dark mb-2">¿Olvidastes tu contraseña?</h4>
-                      <p className="mb-4">Lo comprendemos, a veces las cosas suceden. 
-                      ¡Sólo ingresa tu correo electrónico abajo y te enviaremos un vínculo para que puedas cambiar tu contraseña!</p>
+                      <h4 className="text-dark mb-2">
+                        ¿Olvidastes tu contraseña?
+                      </h4>
+                      <p className="mb-4">
+                        Lo comprendemos, a veces las cosas suceden. ¡Sólo
+                        ingresa tu correo electrónico abajo y te enviaremos un
+                        vínculo para que puedas cambiar tu contraseña!
+                      </p>
                     </div>
                     <form className="user">
                       <div className="form-group">
-                        <input 
-                          className="form-control form-control-user rounded-pill" 
-                          type="email" 
-                          id="exampleInputEmail" 
-                          aria-describedby="emailHelp" 
-                          placeholder="Correo Electrónico..." 
-                          name="email" />
+                        <input
+                          className="form-control form-control-user rounded-pill"
+                          type="email"
+                          id="exampleInputEmail"
+                          aria-describedby="emailHelp"
+                          placeholder="Correo Electrónico..."
+                          name="email"
+                        />
                       </div>
-                      <button 
+                      <button
                         className="btn btn-primary btn-block text-white btn-user rounded-pill py-2"
-                        type="submit">Cambiar Contraseña
+                        type="submit"
+                      >
+                        Cambiar Contraseña
                       </button>
                     </form>
                     <div className="text-center">
-                      <hr/>
-                      <Link className="small" to="/register">¡Crea una cuenta!</Link>
+                      <hr />
+                      <Link className="small" to="/register">
+                        ¡Crea una cuenta!
+                      </Link>
                     </div>
                     <div className="text-center">
-                      <Link className="small" to="/login">¿Ya tienes una cuenta? ¡Ingresa!</Link>
+                      <Link className="small" to="/login">
+                        ¿Ya tienes una cuenta? ¡Ingresa!
+                      </Link>
                     </div>
                   </div>
                 </div>
@@ -51,7 +63,7 @@ function Forgot(){
         </div>
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default Forgot;
